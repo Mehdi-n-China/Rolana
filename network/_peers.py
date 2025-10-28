a1ea705a9ce39b159e85bde651a62b5fcd4f7d3e
Mehdi-n-China <m4hdiouy@gmail.com>
2025-10-28 15:34:35 -0400

    sync

--- network/_peers.py
+++ network/_peers.py
@@ -4,96 +4,84 @@ import sys
 import time
 from tools import InstanceManager, Singleton
 import exceptions
-from collections import deque
+import sqlite3
+
 
 class NetworkManager(Singleton(1)):
     pass
 
 class PeerManager(Singleton(1)):
     def __init__(self) -> None:
-        self.load_from_config()
+        self.conn = sqlite3.connect('../config/peers.db')
+        self.pointer = self.conn.cursor()
+        self.pointer.execute("PRAGMA synchronous = FULL")
+        self.pointer.execute("CREATE TABLE IF NOT EXISTS inbound_peers (peer_id TEXT PRIMARY KEY, peer_ip TEXT)")
+        self.pointer.execute("CREATE TABLE IF NOT EXISTS outbound_peers (peer_id TEXT PRIMARY KEY, peer_ip TEXT)")
+        self.conn.commit()
 
-    def load_from_config(self) -> None:
-        with open("../config/peers.json", "r") as f:
-            if f.read() == "":
-                self.inbound, self.outbound = {}, {}
+        self._load_from_config()
 
-            f.seek(0)
-            data = json.load(f)
-            self.inbound, self.outbound = data.get("inbound_peers", {}), data.get("outbound_peers", {})
+    def _load_from_config(self) -> None:
+        self.pointer.execute("SELECT * FROM inbound_peers")
+        inbound = self.pointer.fetchall()
 
-    @staticmethod
-    def remove_from_config(*, inbound_peers: list = None, outbound_peers: list = None) -> None:
-        global f
-        try:
-            f = open("../config/peers.json", "r")
-            data = json.load(f)
+        self.pointer.execute("SELECT * FROM outbound_peers")
+        outbound = self.pointer.fetchall()
 
-            f = open("../config/peers.json", "w")
-            if inbound_peers:
-                for peer in inbound_peers:
-                    data["inbound_peers"].pop(peer, None)
+        self.inbound = {peer[0]: peer[1] for peer in inbound}
+        self.outbound = {peer[0]: peer[1] for peer in outbound}
 
-            if outbound_peers:
-                for peer in outbound_peers:
-                    data["outbound_peers"].pop(peer, None)
+    def add_peers(self, *, inbound_peers: dict, outbound_peers: dict) -> None:
+        if inbound_peers:
+            self.inbound.update(inbound_peers)
+            self.pointer.executemany("""INSERT INTO inbound_peers (peer_id, peer_ip)
+                                        VALUES (?, ?)
+                                        ON CONFLICT(peer_id) DO UPDATE SET peer_ip = excluded.peer_ip""",
+                                     [(peer_id, peer_ip)
+                                      for peer_id, peer_ip
+                                      in inbound_peers.items()])
 
-            json.dump(data, f)
+        if outbound_peers:
+            self.outbound.update(outbound_peers)
+            self.pointer.executemany("""INSERT INTO outbound_peers (peer_id, peer_ip)
+                                        VALUES (?, ?)
+                                        ON CONFLICT(peer_id) DO UPDATE SET peer_ip = excluded.peer_ip""",
+                                     [(peer_id, peer_ip)
+                                      for peer_id, peer_ip
+                                      in outbound_peers.items()])
 
-        except FileNotFoundError:
-            pass
+        self.conn.commit()
 
-        finally:
-            if f:
-                f.close()
+    def remove_peers(self, *, inbound_peers: list | dict, outbound_peers: list | dict) -> None:
+        for peer in inbound_peers:
+            self.inbound.pop(peer, None)
+        self.pointer.executemany("""DELETE
+                                        FROM inbound_peers
+                                        WHERE peer_id = ?""",
+                                 [(peer,) for peer in inbound_peers])
 
-    @staticmethod
-    def push_to_config(*, new_inbound_peers: dict[str, str] = None,
-                             new_outbound_peers: dict[str, str] = None) -> None:
-        global f
-        try:
-            if new_inbound_peers is None and new_outbound_peers is None:
-                raise exceptions.NoPeerProvidedError(__name__)
-
-            if new_inbound_peers is None:
-                new_inbound_peers = {}
-
-            if new_outbound_peers is None:
-                new_outbound_peers = {}
-
-            f = open("../config/peers.json", "r")
-            if f.read() == "":
-                with open("../config/peers.json", "w") as f:
-                    json.dump({"inbound_peers": {}, "outbound_peers": {}}, f)
-                inbound_peers = {}
-                outbound_peers = {}
-                data = {"inbound_peers": {}, "outbound_peers": {}}
-
-            else:
-                f.seek(0)
-                data = json.load(f)
-                inbound_peers = data.get("inbound_peers", {})
-                outbound_peers = data.get("outbound_peers", {})
-
-            f = open("../config/peers.json", "w")
-            data["inbound_peers"] = {**inbound_peers, **(new_inbound_peers or {})}
-            data["outbound_apeers"] = {**outbound_peers, **(new_outbound_peers or {})}
-
-            json.dump(data, f)
-
-        except exceptions.NetworkError as e:
-            print(e)
-
-        finally:
-            if f:
-                f.close()
-
+        for peer in outbound_peers:
+            self.outbound.pop(peer, None)
+        self.pointer.executemany("""DELETE
+                                        FROM outbound_peers
+                                        WHERE peer_id = ?""",
+                                 [(peer,) for peer in outbound_peers])
+        self.conn.commit()
 
 def main() -> None:
+    import random
+
     p = PeerManager()
-    p.load_from_config()
+    inbound_peers = {str(random.randint(1, 1_000_000)): str(random.randint(1, 1_000_000)) for i in range(1000)}
+    inbound_peers_2 = [str(random.randint(1, 1_000_000)) for i in range(1000)]
+    start = time.perf_counter()
+    p.add_peers(inbound_peers=inbound_peers, outbound_peers=inbound_peers)
+    p.remove_peers(inbound_peers=inbound_peers_2, outbound_peers=inbound_peers_2)
+    stop = time.perf_counter()
+
+    print(stop - start)
+    print(len(p.inbound))
     p.kill()
-    p.load_from_config()
 
 
 
